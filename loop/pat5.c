9f47dc9a6b7127559f3723604cbe6a2612c8379b
ShibamPain <maxxpayne3006@gmail.com>
2025-06-24 14:20:20 +0530

    ouk

--- loop/pat5.c
+++ loop/pat5.c
@@ -1,19 +1,20 @@
-// ****
-// ****
-// ****
-// ****
-
+// * * * * *
+// * * * *
+// * * *
+// * *
+// *
 #include <stdio.h>
 int main()
 {
- int n;
+    int n;
     printf("Enter the number of line :");
     scanf("%d", &n);
-        
+
     for (int i = 0; i < n; i++)
     {
-        for (int k = 0; k < n; k++)
+        for (int j = 0; j < (n - i); j++)
             printf("* ");
+
         printf("\n");
     }
     return 0;
--- loop/pat6.c
+++ loop/pat6.c
@@ -1,18 +1,20 @@
-// ****
-// ****
-// ****
-// ****
+//   * * * *
+//     * * *
+//       * *
+//         *
 
 #include <stdio.h>
 int main()
 {
- int n;
+    int n;
     printf("Enter the number of line :");
     scanf("%d", &n);
-        
+
     for (int i = 0; i < n; i++)
     {
-        for (int k = 0; k < n; k++)
+        for (int j = 0; j <= i; j++)
+            printf("  ");
+        for (int k = 0; k < (n - i); k++)
             printf("* ");
         printf("\n");
     }

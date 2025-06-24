f689c2f089850f2a832e100f47cdbd918d81de62
ShibamPain <maxxpayne3006@gmail.com>
2025-06-24 13:44:34 +0530

    ouk

--- loop/pat4.c
+++ loop/pat4.c
@@ -1,7 +1,8 @@
-//       * 
-//     * *
-//   * * *
-// * * * *
+//    * 
+//   * *
+//  * * *
+// * * * */
+
 #include <stdio.h>
 int main()
 {
@@ -11,12 +12,10 @@ int main()
 
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < (n -i-1); j++)
-            printf("  ");
-
+        for (int j = 0; j < (n - i - 1); j++)
+            printf(" ");
         for (int k = 0; k < i + 1; k++)
             printf("* ");
-
         printf("\n");
     }
     return 0;
